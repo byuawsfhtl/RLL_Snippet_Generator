60bc6188d8bdeb1d1fbd09fbe7c035bd6886f25d
Jackson Roubidoux <jroubidoux1999@gmail.com>
2024-05-22 07:22:11 -0600

    hopefully fixed linting issues

--- tests/snippetGenerator/testSnippetGenerator.py
+++ tests/snippetGenerator/testSnippetGenerator.py
@@ -12,10 +12,10 @@ testFolder = os.path.dirname(current)
 root = os.path.dirname(testFolder)
 sys.path.append(os.path.join(root, "src"))
 
-from SnippetGenerator import (
-    SnippetGenerator,
-    DataFrame_to_Dictionary_converter,
-    CustomException,
+from SnippetGenerator import ( # noqa: E402
+    SnippetGenerator, # noqa: E402
+    DataFrame_to_Dictionary_converter, # noqa: E402
+    CustomException, # noqa: E402
 )  # noqa: E402
 
 
