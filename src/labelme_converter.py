0124555310590a3e0dadc8682e9ba157dedb1d7c
Sarah Stanley <sks239@login04.rc.byu.edu>
2024-07-19 17:51:48 -0600

    Before testing shape["label"]

---
+++ src/labelme_converter.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import json
+import argparse
+import json
+
+
+def read_in_shapes(lm_path):
+    # The main part of a LabelMe file is its 'shapes' list. This function reads in the JSON file and returns that list.
+    try:
+        with open(lm_path, 'r') as file:
+            labelme = json.load(file)
+            return labelme['shapes']
+    except Exception as e:
+        print(f'Error while processing LabelMe file: {e}')
+
+
+def convert_labelme_to_snippet_generator_format(lm_path, out_dir, reel_filename, image_filename):
+    shapes = read_in_shapes(lm_path)
+    rows = []
+    for shape in shapes:
+        print(shape["label"])
+    
+
+        
+
+
+
+if __name__ == "__main__":
+    # Command line parameters
+    parser = argparse.ArgumentParser(description="Convert a LabelMe file to a .tsv file in snippet generator format")
+    parser.add_argument("labelme_filepath", type=str, help="The input file in LabelMe format to be converted to snippet generator format")
+    parser.add_argument("output_directory_path", type=str, help="Where to put the output tsv file")
+    parser.add_argument("reel_filename", type=str, help="The reel tar file the corresponding image came from. Used to fill the 'reel_filename' column of the output tsv.")
+    parser.add_argument("image_filename", type=str, help="The filename of the corresponding image. Used to fill the 'image_filename' column of the output tsv.")
+
+    if len(sys.argv) != 4:
+        print('Usage: <labelme_filepath> <output_directory_path> <reel_filename> <image_filename>')
+    args = parser.parse_args()
+
+    # Check input file path and type
+    lm_path = args.labelme_filepath
+    if not os.path.isfile(lm_path):
+        raise FileNotFoundError(f"LabelMe file does not exist: {lm_path}")
+    CORRECT_EXTENSION = ".json"
+    _, file_extension = os.path.splitext(lm_path)
+    if file_extension != CORRECT_EXTENSION:
+        raise FileNotFoundError(f"File is not a {expected_extension} file.")
+
+    # Set path to output directory
+    out_dir = args.output_directory_path
+    if not os.path.isdir(out_dir):
+        raise FileNotFoundError(f"Output directory does not exist: {out_dir}")
+    print(f'Output set to {out_dir}')
+
+    convert_labelme_to_snippet_generator_format(lm_path, out_dir, args.reel_filename, args.image_filename)
+
+
+    
---
+++ tests/test_labelme_converter/run_labelme_converter_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LABELME="/home/sks239/groups/fslg_census/nobackup/archive/common_tools/snippet_generator/branches/Sarah/RLL_Snippet_Generator/tests/test_labelme_converter/test_labelme.json"
+OUTPUT_DIRECTORY="/home/sks239/groups/fslg_census/nobackup/archive/common_tools/snippet_generator/branches/Sarah/RLL_Snippet_Generator/tests/test_labelme_converter"
+REEL_NAME="test_reel_name_000.tar"
+IMAGE_NAME="test_image_name_000.jpg"
+
